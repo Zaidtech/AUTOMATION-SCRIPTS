4cddf656d037b56b3fc6f5b3ad0de4c84be78304
Zaidtech <mzali@zhcet.ac.in>
2020-07-04 00:36:08 +0530

    Deleted test scripts

--- history_search.py
+++
@@ -1,6 +0,0 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import webbrowser
-search = input ("Enter  what you want to search in history")
- 
-webbrowser.open/'chrome://history/')
