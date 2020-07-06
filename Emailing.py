bdf8e10012851be1255d5546976a3bcdab973544
Zaidtech <mzali@zhcet.ac.in>
2020-07-06 14:59:39 +0530

    Complete mail sending via SMTP

--- Emailing.py
+++ Emailing.py
@@ -1,21 +1,30 @@
+#!/usr/bin/env python3
+
 import smtplib
 
 smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
 #  for gmail --> smpt.gmail.com  PORT 587
-
 # One needs an internet connection else an socket Errno:11044 will be raised
 
 smtpObj.ehlo()
 
 # Starting the TLS Encryption
-
-smtpObj.starttls()
 # This will ensure your SMTP connection is in TLS mode.
+smtpObj.starttls()
 
 MY_MAIL_ADDRESS = input("Enter your valid Gmail address")
-PASSWORD = input("Enter your mail password")
+PASSWORD = input("Enter your above email password")
+RECIEVER_EMAIL_ID = input("Enter the reciever Email Address")
 # Loging in 
 
 smtpObj.login(MY_MAIL_ADDRESS, PASSWORD)
+
+Message = input("Enter the message you want to send")
+for i in range(0,10):
+    smtpObj.sendmail(MY_MAIL_ADDRESS, RECIEVER_EMAIL_ID, f'Subject:\n{Message}')
+
+smtpObj.quit()
+
+
 
 
